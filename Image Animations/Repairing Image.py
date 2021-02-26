308b82efc43f14fa228941bff535c6eeacb2b950
PyExplained <54470013+PyExplained@users.noreply.github.com>
2021-02-26 09:19:48 +0100

    Repairing Image Optimization

--- Image Animations/Repairing Image.py
+++ Image Animations/Repairing Image.py
@@ -1,66 +1,82 @@
-from tkinter import *
+import PIL.Image, PIL.ImageTk
 from random import randint
-import cv2
+from tkinter import *
+import numpy as np
 import time
+import cv2
 
 resolution = 0.1  # 1 = full
+speed = 1
+win_width = 1280
+win_height = 720
 
 tk = Tk()
-canvas = Canvas(tk, width=500, height=500)
+canvas = Canvas(tk, width=win_width, height=win_height)
 canvas.pack()
 
 # Insert file path below
-img = cv2.imread(r'filepath')
+img = cv2.imread(r'C:\Users\manud\OneDrive\Bureaublad\test_img.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 width = len(img[0])
 height = len(img)
 
 
 class Pixel:
-    def __init__(self, canvas, x, y, c, w, h, tx, ty):
-        self.canvas = canvas
+    def __init__(self, x, y, c, s, tx, ty):
         self.color = c
         self.x = x
         self.y = y
         self.target_x = tx
         self.target_y = ty
-        hexa = '#{:02x}{:02x}{:02x}'.format(c[0], c[1], c[2])
-        
-        self.body = self.canvas.create_rectangle(self.x, self.y, self.x+w, self.y+h, fill=hexa, outline='')
+        self.size = s
+
     def move(self):
         x_off = self.target_x - self.x
         y_off = self.target_y - self.y
 
-        if -4 <= x_off <= 4 or -4 <= y_off <= 4:
-            self.canvas.move(self.body, x_off, y_off)
+        if abs(x_off) + abs(y_off) < 2:
             self.x += x_off
             self.y += y_off
-            
-        self.canvas.move(self.body, x_off/25, y_off/25)
-        self.x += x_off/25
-        self.y += y_off/25
+        else:
+            self.x += x_off / (1/speed) * delta_time
+            self.y += y_off / (1/speed) * delta_time
 
 pixels = []
-tk.update()
-w = canvas.winfo_width() / (2 * width * resolution)
-h = canvas.winfo_height() / (2 * height * resolution)
+if width > height:
+    s = win_width / (2 * width * resolution)
+    im_w = win_width / 2
+    im_h = height / width * im_w
+else:
+    s = win_height / (2 * height * resolution)
+    im_h = win_height / 2
+    im_w = width / height * im_h
 
 yy = 0
 for y in range(0, height, round(1/resolution)):
     yy += 1
     xx = 0
     for x in range(0, width, round(1/resolution)):
-        p = Pixel(canvas, randint(0, canvas.winfo_width()), randint(0, canvas.winfo_height()), img[y][x], w, h, 125+xx*w, 125+yy*h)
-        #p = Pixel(canvas, 125 + xx * w, 125 + yy * h, img[y][x], w, h,
-                            # randint(0, canvas.winfo_width()), randint(0, canvas.winfo_height()))
+        p = Pixel(randint(0, win_width), randint(0, win_height), img[y][x], s,
+                  win_width/2-im_w/2+xx*s, win_height/2-im_h/2+yy*s)
+        # p = Pixel(win_width/2-im_w/2+xx*w, win_height/2-im_h/2+yy*h, img[y][x], w, h,
+        #                     randint(0, win_width), randint(0, win_height))
         pixels.append(p)
         xx += 1
 
+last_time = time.time()
+delta_time = 0.07
 while True:
-    try:
-        tk.update()
-        for particle in pixels:
-            particle.move()
-        time.sleep(0.01)
-    except:
-        break
+    canvas.delete('all')
+    image = np.full((win_height, win_width, 3), 228)
+
+    for particle in pixels:
+        particle.move()
+        image[int(particle.y):int(particle.y + particle.size),
+        int(particle.x):int(particle.x + particle.size)] = particle.color
+
+    photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(image.astype(np.uint8)))
+    canvas.create_image(0, 0, image=photo, anchor=NW)
+    tk.update()
+
+    delta_time = time.time() - last_time
+    last_time = time.time()
